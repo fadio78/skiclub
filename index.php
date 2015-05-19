544ef07a290bb12d9399b3a70cf6dfeae4a52a80
floydmoney <slayer57.nc@gmail.com>
2015-05-19 11:38:35 +0200

    page principale

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8" />
+        <title>Se connecter </title>
+    </head>
+    <body>
+        <h2>Bienvenue</h2>
+       
+        <a href ="connexion.php"> se connecter </a>
+
+    </body>
+</html>
